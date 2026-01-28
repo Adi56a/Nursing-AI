16fac6b1d1401774cbc0a815dc83f3d9cae17fa8
Aditya <adityajogdand15@gmail.com>
2026-01-28 11:24:21 +0530

    env

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BioChemistrySyllabus from './pages/BioChemistrySyllabus';
 import ClassificationOfCarb from './pages/BioChemistryPages/Module1/ClassificationOfCarb';
@@ -11,47 +11,34 @@ import RegulationOfBloodGlucose from './pages/BioChemistryPages/Module1/Regulati
 import DiabetesMellitusType1 from './pages/BioChemistryPages/Module1/DiabetesMellitusType1';
 import DiabetesMellitusType2 from './pages/BioChemistryPages/Module1/DiabetesMellitusType2';
 import FattyAcidsClassification from './pages/BioChemistryPages/Module2/FattyAcidsClassification';
-
 import MUFA from './pages/BioChemistryPages/Module2/MUFA';
 import PUFA from './pages/BioChemistryPages/Module2/PUFA';
 import EssentialFattyAcids from './pages/BioChemistryPages/Module2/EssentialFattyAcids';
-
-
+import AIWidget from './components/AIWidget';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-     
-
-       
-
-        {/* Route definitions */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/bio-chem-syllabus" element={<BioChemistrySyllabus />} />
           <Route path="/classification-of-carb" element={<ClassificationOfCarb />} />
           <Route path="/digestion-of-carb" element={<DigestionOfCarb />} />
           <Route path="/absorption-of-carb" element={<AbsorptionOfCarb />} />
-          <Route path="/MetabolicPathwaysOfCarb" element={<MetabolicPathwaysOfCarb/>} />
-          <Route path="/DisordersOfCarb" element={<DisordersOfCarb/>} />
-          <Route path="/RegulationOfBloodGlucose" element={<RegulationOfBloodGlucose/>} />
-          <Route path="/DiabetesMellitusType1" element={<DiabetesMellitusType1/>} />
-          <Route path="/DiabetesMellitusType2" element={<DiabetesMellitusType2/>} />
-
-          {/* module 2 */}
-
-          <Route path="/FattyAcidsClassification" element={<FattyAcidsClassification/>} />
-          <Route path="/MUFA" element={<MUFA/>} />
-          <Route path="/PUFA" element={<PUFA/>} />
-          <Route path="/EssentialFattyAcids" element={<EssentialFattyAcids/>} />
-
-
-
-
-
-        
+          <Route path="/MetabolicPathwaysOfCarb" element={<MetabolicPathwaysOfCarb />} />
+          <Route path="/DisordersOfCarb" element={<DisordersOfCarb />} />
+          <Route path="/RegulationOfBloodGlucose" element={<RegulationOfBloodGlucose />} />
+          <Route path="/DiabetesMellitusType1" element={<DiabetesMellitusType1 />} />
+          <Route path="/DiabetesMellitusType2" element={<DiabetesMellitusType2 />} />
+          <Route path="/FattyAcidsClassification" element={<FattyAcidsClassification />} />
+          <Route path="/MUFA" element={<MUFA />} />
+          <Route path="/PUFA" element={<PUFA />} />
+          <Route path="/EssentialFattyAcids" element={<EssentialFattyAcids />} />
         </Routes>
+
+        {/* AI Nursing Tutor available on every page */}
+        <AIWidget />
       </div>
     </BrowserRouter>
   );
